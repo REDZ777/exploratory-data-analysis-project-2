853743be41841af688f1486821866d2b070bd9f0
Harshavardhanreddy Putta <47238017+REDZ777@users.noreply.github.com>
2020-12-11 16:57:24 +0530

    Create PLOT1.R

---
+++ PLOT1.R
@@ -0,0 +1,23 @@
+library("data.table")
+path <- getwd()
+download.file(url = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip", destfile = paste(path, "dataFiles.zip", sep = "/"))
+
+unzip(zipfile = "dataFiles.zip")
+
+NEI <- data.table::as.data.table(x = readRDS(file = "summarySCC_PM25.rds")) #read data
+NEI[, Emissions := lapply(.SD, as.numeric), .SDcols = c("Emissions")]
+SCC <- data.table::as.data.table(x = readRDS(file = "Source_Classification_Code.rds"))
+
+totalNEI <- NEI[, lapply(.SD, sum, na.rm = TRUE), .SDcols = c("Emissions"), by = year] # summing 
+
+barplot(totalNEI[, Emissions] # plot
+        , names = totalNEI[, year]
+        , xlab = "Years", ylab = "total PM'[2.5]*' emission"
+        , main = "Total PM'[2.5]*' emissions at various years")
+dev.copy(png, file="plot1.png", height=640, width=640)
+dev.off()
+
+
+
+
+
