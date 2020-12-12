3d4b7852d3b7fc03987f665544a28e0050e89993
Harshavardhanreddy Putta <47238017+REDZ777@users.noreply.github.com>
2020-12-12 16:50:27 +0530

    Update PLOT5.R

--- PLOT5.R
+++ PLOT5.R
@@ -8,7 +8,7 @@ df <- subset(SCC, select = c("SCC", "Short.Name"))
 NEI <- merge(NEI, df, by.x="SCC", by.y="SCC", all=TRUE)
 
 PLOT5 <- subset(NEI, fips == "24510" & type =="ON-ROAD", c("Emissions", "year","type")) # plot
-PLOT5 <- aggregate(Emissions ~ year, plot5, sum)
+PLOT5 <- aggregate(Emissions ~ year, PLOT5, sum)
 png(file = "PLOT5.png")
 plot((Emissions / 1000) ~ year, data = PLOT5, type = "l",  xlab = "Year",ylab = "Total emissions (/1000)",main = "Baltimore City PM2.5 Motor Vehicle Emissions", xaxt="n")
 axis(side=1, at=c("1999", "2002", "2005", "2008"))
