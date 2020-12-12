d9c52d5d7f092571738b39ce767ce2eb1ed757e0
Harshavardhanreddy Putta <47238017+REDZ777@users.noreply.github.com>
2020-12-12 15:14:02 +0530

    Update PLOT1.R

--- PLOT1.R
+++ PLOT1.R
@@ -10,10 +10,7 @@ SCC <- data.table::as.data.table(x = readRDS(file = "Source_Classification_Code.
 
 totalNEI <- NEI[, lapply(.SD, sum, na.rm = TRUE), .SDcols = c("Emissions"), by = year] # summing 
 
-barplot(totalNEI[, Emissions] # plot
-        , names = totalNEI[, year]
-        , xlab = "Years", ylab = "total PM'[2.5]*' emission"
-        , main = "Total PM'[2.5]*' emissions at various years")
+barplot(totalNEI[, Emissions], names = totalNEI[, year], xlab = "Years", ylab = "total PM'[2.5]*' emission", main = "Total PM'[2.5]*' emissions at various years") #plot
 dev.copy(png, file="PLOT1.png", height=640, width=640)
 dev.off()
 
